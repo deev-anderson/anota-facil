7ffd9132c11f2b78d6e3381499252d41970be463
deev-anderson <accanderson.barros@gmail.com>
2024-05-20 12:45:44 -0300

    add register component

---
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
+import { register } from "../../actions/userActions";
+import MainScreen from "../../components/MainScreen";
+import "./RegisterScreen.css";
+
+function RegisterScreen({ history }) {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [pic, setPic] = useState(
+    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+  );
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+  const [picMessage, setPicMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const postDetails = (pics) => {
+    if (
+      pics ===
+      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    ) {
+      return setPicMessage("Por favor selecione uma imagem");
+    }
+    setPicMessage(null);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "notezipper");
+      data.append("cloud_name", "piyushproj");
+      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      return setPicMessage("Por favor selecione uma imagem");
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push("/");
+    }
+  }, [history, userInfo]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmpassword) {
+      setMessage("As senhas não coincidem");
+    } else dispatch(register(name, email, password, pic));
+  };
+
+  return (
+    <MainScreen title="CADASTRAR">
+      <div className="loginContainer">
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+        {loading && <Loading />}
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="name"
+              value={name}
+              placeholder="Insira um nome"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              placeholder="Insira um email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="confirmPassword">
+            <Form.Label>Confirme a senha</Form.Label>
+            <Form.Control
+              type="password"
+              value={confirmpassword}
+              placeholder="Confirme a senha"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          {picMessage && (
+            <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
+          )}
+          <Form.Group controlId="pic">
+            <Form.Label>Foto de perfil</Form.Label>
+            <Form.File
+              onChange={(e) => postDetails(e.target.files[0])}
+              id="custom-file"
+              type="image/png"
+              label="Carregar foto de perfil"
+              custom
+            />
+          </Form.Group>
+
+          <Button variant="success" type="submit">
+            Cadastrar
+          </Button>
+        </Form>
+        <Row className="py-3">
+          <Col>
+            Já possui uma conta ? <Link to="/login">Entrar</Link>
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
+}
+
+export default RegisterScreen;
