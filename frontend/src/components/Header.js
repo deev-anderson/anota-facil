651a2984bbd4aadf15a3539c31a7594c114c4990
deev-anderson <accanderson.barros@gmail.com>
2024-05-20 12:42:05 -0300

    add header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import {} from "react-router-dom";
+import { logout } from "../actions/userActions";
+
+function Header({ setSearch }) {
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  useEffect(() => {}, [userInfo]);
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="success"
+      variant="dark"
+      style={{ border: "none" }}
+    >
+      <Container>
+        <Navbar.Brand href="/">NotaFácil</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="m-auto">
+            {userInfo && (
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Pesquisar"
+                  className="mr-sm-2"
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+              </Form>
+            )}
+          </Nav>
+          <Nav>
+            {userInfo ? (
+              <>
+                <Nav.Link href="/mynotes">Minhas Notas</Nav.Link>
+                <NavDropdown
+                  title={`${userInfo.name}`}
+                  id="collasible-nav-dropdown"
+                >
+                  <NavDropdown.Item href="/profile">
+                    Meu Perfil
+                  </NavDropdown.Item>
+
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Sair
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
+            ) : (
+              <Nav.Link href="/login">Entrar</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
